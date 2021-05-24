e21dfa4b73bd93585c644ddedd064566b6a0d759
ITALOTORIBIO <italo_alexander_441@hotmail.com>
2021-05-24 00:10:53 -0500

    mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace learningface_iats.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
